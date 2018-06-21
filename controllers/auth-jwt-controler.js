31bd306855c24f280699b6a8259b91a3e8cbcdf7
Kostiantyn_Rieznik <Kostiantyn_Rieznik@epam.com>
2018-06-21 16:53:49 +0300

    hw5: continue implementing

--- controllers/auth-jwt-controler.js
+++ controllers/auth-jwt-controler.js
@@ -1,13 +1,36 @@
 const jwt = require('jsonwebtoken');
 const users = require('../models/users-model');
+const config = require('../config');
 
 class AuthJWT {
     constructor() {
         this.login = this.login.bind(this);
     }
 
-    login(req, res, next) {
-        res.status('200').send(req.body);
+    login(req, res) {
+        const{name, password} = req.body;
+        const{jwtSecret, expiresIn} = config.jwt;
+
+        users.filter(user => user.name == name && user.password == password).length == 1
+            ? res.status('200')
+                .json({
+                    "code": "200",
+                    "message": "OK",
+                    "data": {
+                        "user": {
+                            "email": "test@gmail.com",
+                            "username": `${name}`
+                        },
+                        "token": jwt.sign({name}, jwtSecret, {expiresIn} )
+                    }
+                })
+            : res.status('401')
+                .json({
+                    "code": "401",
+                    "message": "Not Found",
+                    "data": {},
+                    "token": "null"
+                });
     }
 }
 
