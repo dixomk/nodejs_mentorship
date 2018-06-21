0dc4405838fbe34da92bcfd4d0c2b46855c941e0
dixomk <dixomk@gmail.com>
2018-06-21 08:24:56 +0300

    hw5: implementing auth jwt

--- controllers/users-rest-api-controler.js
+++ controllers/users-rest-api-controler.js
@@ -3,10 +3,15 @@ const users = require('../models/users-model');
 class UsersRestApi {
     constructor() {
         this.getAllUsers = this.getAllUsers.bind(this);
+        this.getByID = this.getByID.bind(this);
     }
 
     getAllUsers(req, res) {
         res.json(users);
+    }
+
+    getByID(req, res) {
+        
     }
 }
 
