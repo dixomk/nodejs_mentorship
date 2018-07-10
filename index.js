4702b2edb185faec0a73de8453f89698bab154d0
dixomk <dixomk@gmail.com>
2018-07-10 07:35:05 +0300

    m_1: added comment A

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require('./app.js');
 const dbProvider = require('./models');
 
 const port = process.env.port || 8081;
-
+// some test comment A
 dbProvider.sequelize.sync()
     .then(() => {
         app.listen(port, () => console.log(`App listening on port ${port} !`));
